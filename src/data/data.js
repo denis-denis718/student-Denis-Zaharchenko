003542703779a2c3ce6b9fc4fc6ab42b159e3e7f
karina-mi <107320679+karina-mi@users.noreply.github.com>
2022-09-09 22:59:53 +0300

    Update data.js

--- src/data/data.js
+++ src/data/data.js
@@ -10,5 +10,12 @@ export const Students = [
     name: 'Peter Ivanov',
     age: 30,
     isMarried: true,
+  },
+  
+  {
+    id: 2,
+    name: 'Mikhieienko Karina',
+    age: 28,
+    isMarried: true,
   }
 ]
