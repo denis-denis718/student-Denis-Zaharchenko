0f5eebf032a3f926f29d719f07374ee908403ba3
Ira <103947302+Ira8694733@users.noreply.github.com>
2022-09-09 23:43:40 +0300

    hw - galtsova

---
+++ src/dz-Ira/data.js
@@ -0,0 +1,20 @@
+export const Students = [
+  {
+    id: 0,
+    name: 'Kate Drawer',
+    age: 25,
+    isMarried: false,
+  },
+  {
+    id: 1,
+    name: 'Peter Ivanov',
+    age: 30,
+    isMarried: true,
+  }
+  {
+    id: 2,
+    name: 'Ira Galtsova',
+    age: 35,
+    isMarried: true,
+  }
+]
